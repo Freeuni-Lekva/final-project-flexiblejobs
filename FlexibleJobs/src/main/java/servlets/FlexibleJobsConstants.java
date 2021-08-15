acd051297999837c1444ce0d555823a441a8c88c
tbarb18 <tbarb18@freeuni.edu.ge>
2021-08-16 01:12:29 +0400

    pull request changes

--- FlexibleJobs/src/main/java/servlets/FlexibleJobsConstants.java
+++ FlexibleJobs/src/main/java/servlets/FlexibleJobsConstants.java
@@ -2,27 +2,16 @@ package servlets;
 
 public class FlexibleJobsConstants {
 
-    public static final String SERVER_NAME = "localhost";
-    public static final int PORT = 3306;
-    public static final String DB_NAME = "FlexibleJobs";
-    public static final String DB_NAME_TEST = "FlexibleJobs_test";
-    public static final String USER = "root";
-    public static final String PASSWORD = "1234";
+    public static final String SERVER_NAME="localhost";
+    public static final int PORT=3306;
+    public static final String DB_NAME="FlexibleJobs";
+    public static final String DB_NAME_TEST="FlexibleJobs_test";
+    public static final String USER="root";
+    public static final String PASSWORD="rootroot";
 
-    public static final String ACCOUNT_ROLE_EMPLOYEE = "employee";
-    public static final String ACCOUNT_ROLE_EMPLOYER = "employer";
-    public static final String ACCOUNT_ROLE_ADMINISTRATOR = "administrator";
-
-    public static final String NO_ACCOUNT = "no_account";
-    public static final String INCORRECT_PASSWORD = "incorrect_password";
-    public static final String EXISTED_ACCOUNT = "account_exists";
-    public static final String NULL_USERNAME = "null_username";
-    public static final String NULL_PASSWORD = "null_password";
-    public static final String NULL_FIRSTNAME = "null_firstname";
-    public static final String NULL_LASTNAME = "null_lastname";
-    public static final String NULL_LIVING_PLACE = "null_living_place";
-    public static final String NULL_HEADING = "null_heading";
-    public static final String NULL_DESCRIPTION = "null_description";
+    public static final String ACCOUNT_ROLE_EMPLOYEE="employee";
+    public static final String ACCOUNT_ROLE_EMPLOYER="employer";
+    public static final String ACCOUNT_ROLE_ADMINISTRATOR="administrator";
 
     public static final String JOB_STATUS_ACTIVE = "Active";
     public static final String JOB_STATUS_IN_PROCESS = "Hired";
